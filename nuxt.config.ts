9efa9a3aebe5c15a65d46248ba227b61c9ebea75
clara seijo <clasei@proton.me>
2026-02-08 19:12:51 +0100

    fix: update tsconfig for Nuxt 3 compatibility and remove future.compatibilityVersion

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  future: {
-    compatibilityVersion: 3
-  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
